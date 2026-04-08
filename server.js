2b106f095b7a19aa95ad45155e7888a0f9002151
lawyerrb2-rgb <lawyerrb2@gmail.com>
2026-04-09 01:53:13 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -42,4 +42,4 @@ app.post('/webhook', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
